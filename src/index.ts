aec0c90789f7b4caec3d37211ec83165e1dc67c5
shelbey25 <sbyousey@crimson.ua.edu>
2026-01-06 14:26:21 -0600

    dynamic port

--- src/index.ts
+++ src/index.ts
@@ -244,7 +244,8 @@ app.post("/verify", async (req: Request, res: Response) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Email verifier API listening on http://localhost:${PORT}`);
+  console.log(`Email verifier API listening on port ${PORT}`);
 });
